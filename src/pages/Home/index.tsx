6314d1d10d03e4f6e87f3c5752de40dc634d7e47
Bruno Oliniski <brunooliniski@gmail.com>
2024-10-21 12:38:55 -0300

    Type for form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,13 +12,10 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
-interface NewCycleFormData {
-  task: string;
-  minutesAmount: number;
-}
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -29,8 +26,7 @@ export function Home() {
   function handleCreateNewCycle(data: NewCycleFormData) {
     // Implement your logic to create a new cycle here
     console.log(data);
-    // Reset form values
-
+    reset();
   }
 
   const task = watch('task')
