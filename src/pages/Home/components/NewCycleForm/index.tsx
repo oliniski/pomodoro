eac13ad5e23fa579a0186e089b34a49daf21a44d
Bruno Oliniski <brunooliniski@gmail.com>
2024-11-20 12:10:03 -0300

    feat: Context

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,41 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
+import { CyclesContext } from "../..";
+import { useContext } from "react";
+import { useFormContext } from "react-hook-form";
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext);  
+  const { register } = useFormContext();
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task" 
+        list="task-suggestions" 
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Trabalhar" />
+        <option value="Estudar" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">Durante</label>
+      <MinutesAmountInput 
+        type="number" 
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true})}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
